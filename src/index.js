43bf61c86f865835a189cfe269e694253c92dc9e
Gopinath <gopinath.yarru@yash.com>
2019-10-10 16:20:16 +0530

    index changes

--- src/index.js
+++ src/index.js
@@ -4,45 +4,45 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 //import Day1 from './1/day-1';
-import Day2 from './2/Day-2';
+//import Day2 from './2/Day-2';
 //import Day3 from './3/Day-3';
-// import Day4 from './4/Day-4';
+//import Day4 from './4/Day-4';
 //import Day5 from './5/Day-5';
 //import Day6 from './6/Day6';
-// import Day7 from './7/Day7';
-// import App from './Routing/App';
-// import Home from './Routing/Home';
-// import About from './Routing/About';
-// import Contact from './Routing/Contact';
-// import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+//import Day7 from './7/Day7';
+import App from './Routing/App';
+import Home from './Routing/Home';
+import About from './Routing/About';
+import Contact from './Routing/Contact';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
-// const routing = (
-//     <Router>
-//         <div>
-//             <ul>
-//                 <li>
-//                     <Link to='/'>App</Link>
-//                 </li>
-//                 <li>
-//                     <Link to='/Home'>Home</Link>
-//                 </li>
-//                 <li>
-//                     <Link to='/About'>About</Link>
-//                 </li>
-//                 <li>
-//                     <Link to='/Contact'>Contact</Link>
-//                 </li>
-//             </ul>
-//             <Route exact path='/' component={App} />
-//             <Route path='/About' component={About} />
-//             <Route path='/Home' component={Home} />
-//             <Route path='/Contact' component={Contact} />
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to='/'>App</Link>
+                </li>
+                <li>
+                    <Link to='/Home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/About'>About</Link>
+                </li>
+                <li>
+                    <Link to='/Contact'>Contact</Link>
+                </li>
+            </ul>
+            <Route exact path='/' component={App} />
+            <Route path='/About' component={About} />
+            <Route path='/Home' component={Home} />
+            <Route path='/Contact' component={Contact} />
 
-//         </div >
-//     </Router >
-// )
+        </div >
+    </Router >
+)
 
-ReactDOM.render(<Day2/>, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 // ReactDOM.render(<Day1/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
